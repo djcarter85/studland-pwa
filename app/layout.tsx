19d7a347537c4206d2e0e391d17d6accb37944cf
Dan Carter <djcarter85@gmail.com>
2024-01-07 20:51:30 +0000

    Make it a PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Studland Site",
+  manifest: "manifest.json",
+  themeColor: "#14b8a6",
 };
 
 export default function RootLayout({
