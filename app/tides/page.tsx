336c23e5486de9353271e0314e30b7fce894e8d2
Dan Carter <djcarter85@gmail.com>
2024-01-08 19:06:37 +0000

    Add basic tides layout

--- app/tides/page.tsx
+++ app/tides/page.tsx
@@ -3,9 +3,9 @@
 import useSWR from "swr";
 
 export default function Tides() {
-  const fetcher = (url: string) => fetch(url).then((res) => res.text());
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const key =
-    "https://raw.githubusercontent.com/djcarter85/studland/master/_data/tides/2024-01-08.csv";
+    "https://raw.githubusercontent.com/djcarter85/studland-data/main/data/tides.json";
   const { data, error, isLoading } = useSWR(key, fetcher);
 
   if (error) return <div>failed to load</div>;
@@ -14,7 +14,24 @@ export default function Tides() {
   return (
     <div>
       <h1>Tides</h1>
-      {data}
+      <table>
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Type</th>
+            <th>Height</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.tides.map((t: any) => (
+            <tr key={t.timestamp}>
+              <td>{t.timestamp}</td>
+              <td>{t.type}</td>
+              <td>{t.height} m</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
