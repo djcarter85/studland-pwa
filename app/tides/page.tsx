9c847158aebedce898678ef2d8814516c407900d
Dan Carter <djcarter85@gmail.com>
2024-01-08 13:27:52 +0000

    Use SWR for tide data loading

--- app/tides/page.tsx
+++ app/tides/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import useSWR from "swr";
+
 export default function Tides() {
-  return <div>Tides</div>;
+  const fetcher = (url: string) => fetch(url).then((res) => res.text());
+  const key =
+    "https://raw.githubusercontent.com/djcarter85/studland/master/_data/tides/2024-01-08.csv";
+  const { data, error, isLoading } = useSWR(key, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <div>
+      <h1>Tides</h1>
+      {data}
+    </div>
+  );
 }
