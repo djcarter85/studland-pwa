add16c513a3a0f7e84b1a558ec080b1a06427cf7
Dan Carter <djcarter85@gmail.com>
2024-01-08 23:01:03 +0000

    Build out a rough UI for tide data

--- app/tides/page.tsx
+++ app/tides/page.tsx
@@ -1,6 +1,32 @@
 "use client";
 
+import { faAnglesDown, faAnglesUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useSWR from "swr";
+
+function TideRow({
+  type,
+  timestamp,
+  height,
+}: {
+  type: string;
+  timestamp: string;
+  height: number;
+}) {
+  // TODO: properly parse and convert to browser local time zone
+  const time = timestamp.substring(11, 16);
+
+  return (
+    <tr className="text-center border-b">
+      <td className="py-1">
+        <FontAwesomeIcon icon={type === "low" ? faAnglesDown : faAnglesUp} />
+      </td>
+      <td className="py-1 capitalize">{type}</td>
+      <td className="py-1">{time}</td>
+      <td className="py-1">{height.toFixed(1)} m</td>
+    </tr>
+  );
+}
 
 export default function Tides() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -13,22 +39,16 @@ export default function Tides() {
 
   return (
     <div>
-      <h1>Tides</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Type</th>
-            <th>Height</th>
-          </tr>
-        </thead>
+      <h1 className="text-3xl text-center font-bold mb-4">Tides at Studland Bay</h1>
+      <table className="w-full border-t">
         <tbody>
           {data.tides.map((t: any) => (
-            <tr key={t.timestamp}>
-              <td>{t.timestamp}</td>
-              <td>{t.type}</td>
-              <td>{t.height} m</td>
-            </tr>
+            <TideRow
+              key={t.timestamp}
+              type={t.type}
+              timestamp={t.timestamp}
+              height={t.height}
+            />
           ))}
         </tbody>
       </table>
