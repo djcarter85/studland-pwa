8d46b1cc96de27cc2e376ebda333d89e881640b9
Dan Carter <djcarter85@gmail.com>
2024-01-08 23:00:53 +0000

    Change base font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Nav from "@/app/components/nav";
 import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import clsx from "clsx";
-import { usePathname } from "next/navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+const baseFont = DM_Sans({ subsets: ["latin"], weight: ["300", "700"] });
 
 export const metadata: Metadata = {
   title: "Studland Site",
@@ -30,7 +29,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, "bg-zinc-50")}>
+      <body className={clsx(baseFont.className, "bg-zinc-50")}>
         <Nav />
         <main className="mx-auto max-w-xl p-3 text-teal-950 mb-safe-offset-16">
           {children}
