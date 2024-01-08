0dca223dc3d8e451db1950d27379856c8d4f36a1
Dan Carter <djcarter85@gmail.com>
2024-01-08 09:35:46 +0000

    Improve weather icon

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,7 @@
 import {
   faCalendar,
   faCircleInfo,
-  faHome,
+  faCloudSunRain,
   faMap,
   faWater,
 } from "@fortawesome/free-solid-svg-icons";
@@ -11,7 +11,7 @@ export default function Nav() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 w-full border-t bg-zinc-50 shadow-[0px_0px_6px_2px_#edf2f7] pb-safe">
       <div className="mx-auto flex h-16 max-w-xl flex-row items-center justify-around">
-        <NavLink title="Weather" href="/weather" icon={faHome} />
+        <NavLink title="Weather" href="/weather" icon={faCloudSunRain} />
         <NavLink title="Tides" href="/tides" icon={faWater} />
         <NavLink title="Map" href="/map" icon={faMap} />
         <NavLink title="Calendar" href="/calendar" icon={faCalendar} />
