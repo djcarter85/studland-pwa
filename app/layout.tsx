65464548f9eb4fbef22c28fa4b659a57b1536076
Dan Carter <djcarter85@gmail.com>
2024-01-12 14:08:46 +0000

    Tweak warning banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Nav from "@/app/components/nav";
 import clsx from "clsx";
-import { ExclamationCircle } from "react-bootstrap-icons";
+import { Android2, Apple, ExclamationCircle } from "react-bootstrap-icons";
 
 const baseFont = DM_Sans({ subsets: ["latin"], weight: ["300", "700"] });
 
@@ -28,18 +28,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={clsx(baseFont.className, "bg-gray-50")}>
         <Nav />
-        <div className="pwa:hidden bg-orange-100">
-          <div className="mx-auto flex max-w-xl flex-row gap-6 p-4 items-center border-b-2 border-orange-600">
-            <div className="text-3xl text-orange-600">
-              <ExclamationCircle />
-            </div>
-            <div className="flex flex-col gap-2 text-sm font-bold text-orange-900">
-              <p>This website works best when installed as an app.</p>
-              <p>
-                To do this on an iPhone, click on the &quot;Share&quot; button, then click
+        <div className="bg-orange-100 pwa:hidden">
+          <div className="mx-auto flex max-w-xl flex-col gap-2 border-b-2 border-orange-600 p-4 text-sm font-bold text-orange-900">
+            <p>This website works best when installed as an app.</p>
+            <p>
+              <Apple className="mr-1 inline align-baseline" />
+              <span>
+                iPhone: click on the &quot;Share&quot; button, then click
                 &quot;Add to Home Screen&quot;.
-              </p>
-            </div>
+              </span>
+            </p>
+            <p>
+              <Android2 className="mr-1 inline align-baseline" />
+              <span>Android: instructions coming soon ...</span>
+            </p>
           </div>
         </div>
         <main className="mx-auto max-w-xl p-2 text-gray-900 mb-safe-offset-16">
