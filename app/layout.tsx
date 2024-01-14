dbdf007228cfb2bf03ab5f65a40bb19c2b7e8bf9
Dan Carter <djcarter85@gmail.com>
2024-01-14 15:13:58 +0000

    Refactor: extract component

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,27 @@ export const viewport: Viewport = {
   viewportFit: "cover",
 };
 
+function PwaWarning() {
+  return (
+    <div className="bg-orange-100 pwa:hidden">
+      <div className="mx-auto flex max-w-xl flex-col gap-2 border-b-2 border-orange-600 p-4 text-sm font-bold text-orange-900">
+        <p>This website works best when installed as an app.</p>
+        <p>
+          <Apple className="mr-1 inline align-baseline" />
+          <span>
+            iPhone: click on the &quot;Share&quot; button, then click &quot;Add
+            to Home Screen&quot;.
+          </span>
+        </p>
+        <p>
+          <Android2 className="mr-1 inline align-baseline" />
+          <span>Android: instructions coming soon ...</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -28,22 +49,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={clsx(baseFont.className, "bg-gray-50")}>
         <Nav />
-        <div className="bg-orange-100 pwa:hidden">
-          <div className="mx-auto flex max-w-xl flex-col gap-2 border-b-2 border-orange-600 p-4 text-sm font-bold text-orange-900">
-            <p>This website works best when installed as an app.</p>
-            <p>
-              <Apple className="mr-1 inline align-baseline" />
-              <span>
-                iPhone: click on the &quot;Share&quot; button, then click
-                &quot;Add to Home Screen&quot;.
-              </span>
-            </p>
-            <p>
-              <Android2 className="mr-1 inline align-baseline" />
-              <span>Android: instructions coming soon ...</span>
-            </p>
-          </div>
-        </div>
+        <PwaWarning />
         <main className="mx-auto max-w-xl p-2 text-gray-900 mb-safe-offset-16">
           {children}
         </main>
