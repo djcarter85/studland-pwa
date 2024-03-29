0efd86429f37325b3adbeb57dc7b14fb09c85767
Dan Carter <djcarter85@gmail.com>
2024-03-29 15:30:52 +0000

    Add time example to calendar page

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,3 +1,15 @@
+import useData from "../../hooks/useData";
+
 export default function Calendar() {
-  return <div>Calendar</div>;
+  const { data, isLoading } = useData(
+    "http://worldtimeapi.org/api/timezone/Europe/London"
+  );
+
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <p>{data && data.utc_datetime.substring(11, 19)}</p>
+      <p>{isLoading && "loading"}</p>
+    </div>
+  );
 }
