2ad7a1c186c02ea7a6de4c599dede2281e9ed021
Dan Carter <djcarter85@gmail.com>
2024-04-30 21:52:26 +0100

    refactor: use enum on Tides page

--- src/pages/tides-page.tsx
+++ src/pages/tides-page.tsx
@@ -14,7 +14,7 @@ import { z } from "zod";
 import { dateSchema } from "../schemas/date-schema";
 
 const tideSchema = z.object({
-  type: z.string(),
+  type: z.enum(["Low", "High"]),
   time: z.string(),
   height: z.string(),
 });
@@ -28,17 +28,7 @@ const tideDateSchema = z.object({
 
 const tidesSchema = z.object({ dates: z.array(tideDateSchema) });
 
-function TideRow({
-  date,
-  type,
-  time,
-  height,
-}: {
-  date: DateTime | undefined;
-  type: string;
-  time: string;
-  height: string;
-}) {
+function TideRow({ date, tide }: { date: DateTime | undefined; tide: Tide }) {
   return (
     <tr className="text-center odd:bg-gray-100 odd:dark:bg-gray-700">
       <td className="py-2 text-lg font-bold">
@@ -46,11 +36,11 @@ function TideRow({
       </td>
       <td
         className={clsx("border-l-8 py-2 text-3xl", {
-          "border-violet-300/80 dark:border-violet-300/60": type === "Low",
-          "border-sky-300/80 dark:border-sky-300/60": type === "High",
+          "border-violet-300/80 dark:border-violet-300/60": tide.type === "Low",
+          "border-sky-300/80 dark:border-sky-300/60": tide.type === "High",
         })}
       >
-        {time}
+        {tide.time}
       </td>
       <td className="flex flex-col items-center py-2">
         <span
@@ -58,19 +48,19 @@ function TideRow({
             "inline-flex w-min flex-row items-center gap-2 rounded-full px-2 py-1",
             {
               "bg-violet-200 text-violet-800 dark:bg-violet-300/70 dark:text-violet-950":
-                type === "Low",
+                tide.type === "Low",
               "bg-sky-200 text-sky-800 dark:bg-sky-300/70 dark:text-sky-950":
-                type === "High",
+                tide.type === "High",
             }
           )}
         >
           <span>
-            {type === "Low" ? <ChevronDoubleDown /> : <ChevronDoubleUp />}
+            {tide.type === "Low" ? <ChevronDoubleDown /> : <ChevronDoubleUp />}
           </span>
-          <span className="text-xs font-bold uppercase">{type}</span>
+          <span className="text-xs font-bold uppercase">{tide.type}</span>
         </span>
         <span className="font-bold text-gray-500 dark:text-gray-300">
-          {height}
+          {tide.height}
         </span>
       </td>
     </tr>
@@ -81,13 +71,7 @@ function DateBlock({ date, tides }: { date: DateTime; tides: Tide[] }) {
   return (
     <>
       {tides.map((t, index) => (
-        <TideRow
-          key={t.time}
-          date={index == 0 ? date : undefined}
-          type={t.type}
-          time={t.time}
-          height={t.height}
-        />
+        <TideRow key={t.time} date={index == 0 ? date : undefined} tide={t} />
       ))}
     </>
   );
