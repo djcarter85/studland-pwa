e153ff2157216f707d2e9f8e1a74a9efc67cde24
Dan Carter <djcarter85@gmail.com>
2024-05-21 16:54:13 +0100

    refactor: extract component

--- src/components/last-updated-section.tsx
+++ src/components/last-updated-section.tsx
@@ -2,6 +2,15 @@ import { DateTime } from "luxon";
 import { ArrowRepeat, CheckCircle } from "react-bootstrap-icons";
 import { LoadingState } from "../types/loading-state";
 
+const LoadingIndicator = ({ loadingState }: { loadingState: LoadingState }) => {
+  switch (loadingState) {
+    case "loading":
+      return <ArrowRepeat className="animate-spin" />;
+    case "loaded":
+      return <CheckCircle className="text-teal-500 dark:text-teal-400" />;
+  }
+};
+
 export default function LastUpdatedSection({
   loadingState,
   lastUpdatedUtc,
@@ -17,11 +26,7 @@ export default function LastUpdatedSection({
   return (
     <div className="my-3 flex flex-row items-center gap-3 px-3">
       <div className="text-lg">
-        {loadingState == "loading" ? (
-          <ArrowRepeat className="animate-spin" />
-        ) : (
-          <CheckCircle className="text-teal-500 dark:text-teal-400" />
-        )}
+        <LoadingIndicator loadingState={loadingState} />
       </div>
       <div className="text-sm">
         Last updated {dateTime.toRelative({ style: "long" })}
