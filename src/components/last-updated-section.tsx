1f85a1171bc1006084f4440856829330483dd2b4
Dan Carter <djcarter85@gmail.com>
2024-05-21 16:57:20 +0100

    refactor: extract component

--- src/components/last-updated-section.tsx
+++ src/components/last-updated-section.tsx
@@ -2,6 +2,15 @@ import { DateTime } from "luxon";
 import { ArrowRepeat, CheckCircle } from "react-bootstrap-icons";
 import { LoadingState } from "../types/loading-state";
 
+const LoadingStateText = ({ lastUpdatedUtc }: { lastUpdatedUtc: string }) => {
+  // Ensure the time isn't in the future by subtracting a millisecond.
+  const dateTime = DateTime.fromISO(lastUpdatedUtc).plus({
+    milliseconds: -1,
+  });
+
+  return <>Last updated {dateTime.toRelative({ style: "long" })}</>;
+};
+
 const LoadingIndicator = ({ loadingState }: { loadingState: LoadingState }) => {
   switch (loadingState) {
     case "loading":
@@ -18,18 +27,13 @@ export default function LastUpdatedSection({
   loadingState: LoadingState;
   lastUpdatedUtc: string;
 }) {
-  // Ensure the time isn't in the future by subtracting a millisecond.
-  const dateTime = DateTime.fromISO(lastUpdatedUtc).plus({
-    milliseconds: -1,
-  });
-
   return (
     <div className="my-3 flex flex-row items-center gap-3 px-3">
       <div className="text-lg">
         <LoadingIndicator loadingState={loadingState} />
       </div>
       <div className="text-sm">
-        Last updated {dateTime.toRelative({ style: "long" })}
+        <LoadingStateText lastUpdatedUtc={lastUpdatedUtc} />
       </div>
     </div>
   );
