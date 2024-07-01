4e06a15335ba57680ac007d9c78130448d72f43c
Dan Carter <djcarter85@gmail.com>
2024-07-01 13:07:22 +0100

    feat: simplify PWA install warning (#42)

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import clsx from "clsx";
-import { Android2, Apple, BoxArrowUp, PlusSquare } from "react-bootstrap-icons";
 import { useTernaryDarkMode } from "usehooks-ts";
 import Nav from "./components/nav";
 import { useEffect } from "react";
@@ -9,18 +8,9 @@ function PwaWarning() {
   return (
     <div className="bg-orange-100 dark:bg-gray-600 pwa:hidden">
       <div className="mx-auto flex max-w-xl flex-col gap-2 border-b-2 border-orange-600 p-4 text-sm font-bold text-orange-900 dark:font-normal dark:text-orange-200">
-        <p>This website works best when installed as an app.</p>
         <p>
-          <Apple className="mr-1 inline align-baseline" />
-          <span>iOS: tap</span>
-          <BoxArrowUp className="mx-1 inline align-baseline" />
-          <span>Share, then </span>
-          <PlusSquare className="mx-1 inline align-baseline" />
-          <span>Add to Home Screen.</span>
-        </p>
-        <p>
-          <Android2 className="mr-1 inline align-baseline" />
-          <span>Android: instructions coming soon ...</span>
+          This website works best when installed as an app. You can do this by
+          choosing "Add to home screen" from your browser's menu.
         </p>
       </div>
     </div>
